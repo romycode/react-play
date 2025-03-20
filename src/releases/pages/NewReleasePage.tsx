b2d26ccf1d1be19a70d50886e9492847d4c981ee
romycode <romycode@gmail.com>
2025-03-20 14:54:41 +0100

    remove internal state management for input and validations and move to form hook

--- src/releases/pages/NewReleasePage.tsx
+++ src/releases/pages/NewReleasePage.tsx
@@ -3,33 +3,69 @@ import {useState} from "react";
 import useAppNavigation from "~/shared/hooks/useAppNavigation.tsx";
 import Input from "~/shared/components/Input.tsx";
 
+type Release = { title: string; };
+type ReleaseErrors = Record<keyof Release, string | null>
+
+
+function useNewRelease() {
+    const [release, setRelease] = useState<Release>({title: ""});
+    const [errors, setErrors] = useState<ReleaseErrors>({
+        title: null
+    });
+
+    const validateRelease = (newRelease: Partial<Release>): boolean => {
+        let valid = false;
+        let error: string | null = null
+
+        if (newRelease.title !== undefined) {
+            error = validateReleaseField("title", newRelease.title);
+            setErrors(prev => ({...prev, title: error}));
+            valid = false
+        }
+
+        return valid
+    };
+
+    const validateReleaseField = (field: string, value: string): string => {
+        switch (field) {
+            case "title":
+                if (value === "") return "Title is required";
+                return ""
+            default:
+                return ""
+        }
+    };
+
+    const updateRelease = (release: Partial<Release>) => {
+        validateRelease(release)
+        setRelease(prevState => ({...prevState, ...release} satisfies Release))
+    };
+
+    return {release, errors, updateRelease, validateRelease};
+}
+
 export default function NewReleasePage() {
     const {navigateBack} = useAppNavigation();
 
-    const [release, setRelease] = useState<{ title: string; }>({title: ""});
-
-    const validateReleaseTitle = (title: string): string => {
-        if (title === "") return "Title is required";
-        return ""
-    };
-
-    const updateReleaseTitle = (title: string) => {
-        setRelease(prevState => ({...prevState, title}))
-    };
+    const {release, errors, updateRelease, validateRelease} = useNewRelease();
 
     return <>
         <nav>
             <button type="button" onClick={navigateBack}>{"<- Back"}</button>
         </nav>
         <h1>New Release</h1>
-        <form>
+        <form action={() => {
+            validateRelease(release)
+        }}>
             <Input
-                onChange={updateReleaseTitle}
-                value={release.title}
-                defaultValue=""
                 label="Title"
-                validate={validateReleaseTitle}
+                error={errors.title}
+                value={release.title}
+                onChange={(title) => {
+                    updateRelease({title})
+                }}
             />
+            <button type="submit">Validate</button>
         </form>
     </>
 }
--- src/shared/components/Input.tsx
+++ src/shared/components/Input.tsx
@@ -1,34 +1,20 @@
-import {ChangeEvent, useCallback, useState} from "react";
+import {ChangeEvent} from "react";
 
 interface props {
-    label: string;
-    defaultValue: string;
     value: string;
+    error: string|null;
+    label: string;
     onChange: (value: string) => void;
-    validate?: (value: string) => string;
 }
 
-export default function Input({label, defaultValue, value, onChange, validate}: props) {
-    if (validate === undefined) validate = () => ""
-    const initialError = defaultValue === value ? null : validate(value);
-
-    const [error, setError] = useState(initialError);
-
-    const handleChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
-        const data = event.target.value;
-
-        const error = validate(data);
-        if (error) {
-            setError(error)
-            return
-        }
-        setError(null)
-        onChange(data)
-    }, [onChange, validate])
+export default function Input({label, value, error, onChange}: props) {
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange(e.target.value);
+    }
 
     return <>
         <label htmlFor={label}></label>
-        <input id={label} onChange={handleChange}/>
+        <input id={label} onChange={handleChange} value={value}/>
         {
             error !== null
                 ? <p>{error}</p>
